969bf10a6a9772659f84749db6c4ebf825c28a74
CharlyPierce <103223806+CharlyPierce@users.noreply.github.com>
2023-11-06 07:30:27 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,25 @@
+function skillsMember() {
+    let skills = [
+        {
+            id: 1,
+            name: "Javascript",
+            experience: "Intermediate"
+        },
+        {
+            id: 2,
+            name: "HTML",
+            experience: "Intermediate"
+        },
+        {
+            id: 3,
+            name: "CSS",
+            experience: "Intermediate"
+        },
+        {
+            id: 4,
+            name: "React",
+            experience: "Beginner"
+        }
+    ];
+    return skills;
+}
